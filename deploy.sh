502ac8c36aef56a0eb7954fb549c326f11508e42
akihiro <takiaki1001e@gmail.com>
2022-10-15 13:47:13 +0900

    スロークエリログ設定変更

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ git pull origin main
 cd /home/isucon/isuumo/webapp/go
 go build
 
-# # ハードリンク貼り直し
-# sudo rm -f /etc/mysql/mariadb.conf.d/50-server.cnf
-# cd /etc/mysql/mariadb.conf.d
-# sudo ln /home/isucon/webapp/configs/50-server.cnf
+# ハードリンク貼り直し
+sudo rm -f /etc/mysql/mysql.conf.d/mysqld.cnf
+cd /etc/mysql/mysql.conf.d
+sudo ln /home/isucon/isuumo/webapp/configs/mysqld.cnf
 
 # サービス再起動
 sudo systemctl restart nginx
@@ -20,5 +20,5 @@ sudo systemctl restart mysql
 sudo systemctl restart isuumo.go
 
 # ログクリア
-# sudo truncate /var/log/mysql/mysql-slow.log --size 0
+sudo truncate /var/log/mysql/mysql-slow.log --size 0
 sudo truncate /var/log/nginx/access.log --size 0
--- summary.sh
+++ summary.sh
@@ -10,4 +10,4 @@ sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/condition/.*,/api/isu/.*/
 sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/condition/.*,/api/isu/.*/graph,/api/isu/.*/icon,/api/isu/.*,/isu/.*/graph,/isu/.*/condition,/isu/.*' --sort avg -r > /tmp/alp-grouped-avg.txt
 
 # スロークエリ解析結果を出力
-# sudo pt-query-digest /var/log/mysql/mysql-slow.log > /tmp/pt-query-digest.txt
+sudo pt-query-digest /var/log/mysql/mysql-slow.log > /tmp/pt-query-digest.txt
