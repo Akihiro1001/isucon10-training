c3cfa92c41f9570f5b8d963d6184c03f86854a16
akihiro <takiaki1001e@gmail.com>
2022-10-15 14:33:56 +0900

    summary.shの正規表現修正

--- summary.sh
+++ summary.sh
@@ -5,9 +5,9 @@
 sudo cat /var/log/nginx/access.log | alp ltsv > /tmp/alp.txt
 # 正規表現でグルーピングしたalp出力
 # 合計降順
-sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/condition/.*,/api/isu/.*/graph,/api/isu/.*/icon,/api/isu/.*,/isu/.*/graph,/isu/.*/condition,/isu/.*' --sort sum -r > /tmp/alp-grouped-sum.txt
+sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/chair/[0-9]+,/api/chair,/api/chair/search,/api/chair/low_priced,/api/chair/search/condition,/api/chair/buy/[0-9]+,/api/estate/[0-9]+,/api/estate,/api/estate/search,/api/estate/low_priced,/api/estate/req_doc/[0-9]+,/api/estate/nazotte,/api/estate/search/condition,/api/recommended_estate/[0-9]+' --sort sum -r > /tmp/alp-grouped-sum.txt
 # 平均降順
-sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/condition/.*,/api/isu/.*/graph,/api/isu/.*/icon,/api/isu/.*,/isu/.*/graph,/isu/.*/condition,/isu/.*' --sort avg -r > /tmp/alp-grouped-avg.txt
+sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/chair/[0-9]+,/api/chair,/api/chair/search,/api/chair/low_priced,/api/chair/search/condition,/api/chair/buy/[0-9]+,/api/estate/[0-9]+,/api/estate,/api/estate/search,/api/estate/low_priced,/api/estate/req_doc/[0-9]+,/api/estate/nazotte,/api/estate/search/condition,/api/recommended_estate/[0-9]+' --sort avg -r > /tmp/alp-grouped-avg.txt
 
 # スロークエリ解析結果を出力
 sudo pt-query-digest /var/log/mysql/mysql-slow.log > /tmp/pt-query-digest.txt
