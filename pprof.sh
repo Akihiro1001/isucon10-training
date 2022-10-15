8dac420c6d6dffd8454c96d9e6e02d4a8770388f
akihiro <takiaki1001e@gmail.com>
2022-10-15 14:18:20 +0900

    pprof.sh追加

---
+++ pprof.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 分析データのリセット
+rm -rf /home/isucon/pprof/
+
+# 公開サーバーのプロセス停止
+kill -9 $(lsof -t -i :8080)
+
+# 公開サーバー起動、分析データの収集
+nohup go tool pprof -http=0.0.0.0:8080 /home/isucon/isuumo/webapp/go/isuumo http://localhost:6060/debug/pprof/profile &
